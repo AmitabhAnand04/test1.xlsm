c713a784db1de202d7a4d41b1da2c366484fb6f5
Sk Nasir Hussain <nasir_h@pursuitsoftware.biz>
2024-05-06 15:50:25 +0530

    New module added by Sk Nasir Hussain

---
+++ Module1.vb
@@ -0,0 +1,7 @@
+Option Explicit
+
+Sub saveas()
+
+    ThisWorkbook.saveas ("C:\Users\SkNasirHussain\pursuit software development pvt. ltd\Sujit Sarkar - EXCELGEN\" & ActiveWorkbook.Name)
+    
+End Sub
